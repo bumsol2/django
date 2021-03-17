e78a46a5973da01b268ab75dc2786750182340a3
bumsol2 <bumsol2@gmail.com>
2021-03-17 18:28:39 +0900

    리스트, 튜플(3)

--- 0317.py
+++ 0317.py
@@ -75,3 +75,19 @@ d = (10, 100, ('a', 'b', 'c'))
 print(c[2])
 print(c[3])
 print(d[2][1])
+print(d[2:])
+print(d[2][0:2])
+
+print(c + d)
+print(c * 3)
+print()
+print()
+
+# 튜플 함수
+
+z = (5, 2, 1, 3, 1)
+
+print(z)
+print(3 in z)
+print(z.index(5))
+print(z.count(1))
