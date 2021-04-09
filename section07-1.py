67f63d3940edd411db7b6c47eac44670f4a55333
bumsol2 <bumsol2@gmail.com>
2021-04-09 20:19:33 +0900

    클래스 어렵도다 오버라이딩 하면 자식 클래스만 나오게 할수 있고, super() 쓰면 부모랑 자식 둘다 나오네..어렵다 어려워..

--- section07-1.py
+++ section07-1.py
@@ -5,7 +5,7 @@
 # 클래스, 인스턴스 차이 중요
 # 네임스페이스 : 객체를 인스턴스화 할 떄 저장된 공간
 # 클래스 변수 : 직접 사용 가능, 객체보다 먼저 생성
-# 인스턴스 변수 :  객체마다 별도로 존재, 인스턴스 생성후 
+# 인스턴스 변수 :  객체마다 별도로 존재, 인스턴스 생성후
 
 # 선언
 # class 클래스명:
@@ -23,7 +23,8 @@ class UserInfo:
     def user_info_p(self):
         print("Name : ", self.name)
 
-# 네임 스페이스 
+
+# 네임 스페이스
 user1 = UserInfo("Sol")
 print(user1.name)
 user1.user_info_p()
@@ -38,12 +39,16 @@ print(user2.__dict__)
 
 # 예제2
 # self의 이해
+
+
 class SelfTest():
     def function1():
         print('function1 called')
+
     def function2(self):
         print(id(self))
         print('function2 called')
+
 
 self_test = SelfTest()
 # self_test.function1()
@@ -56,14 +61,18 @@ SelfTest.function2(self_test)
 # 예제3
 # 클래스 변수 self없고, 인스턴스 변수 Self있따.
 
+
 class WareHouse:
     # 클래스 변수
     stock_num = 0
+
     def __init__(self, name):
         self.name = name
         WareHouse.stock_num += 1
+
     def __del__(self):
         WareHouse.stock_num -= 1
+
 
 user1 = WareHouse('Kim')
 user2 = WareHouse('Park')
@@ -72,24 +81,17 @@ user3 = WareHouse('Lee')
 print(user1.__dict__)
 print(user2.__dict__)
 print(user3.__dict__)
-print(WareHouse.__dict__) #클래스 네임스페이스, 클래스 변수(공유)
+print(WareHouse.__dict__)  # 클래스 네임스페이스, 클래스 변수(공유)
 
 print(user1.name)
 print(user2.name)
 print(user3.name)
 
-print(user1.stock_num) 
-print(user2.stock_num) 
-print(user3.stock_num) 
+print(user1.stock_num)
+print(user2.stock_num)
+print(user3.stock_num)
 
 del user1
 
-print(user2.stock_num) 
-print(user3.stock_num) 
-
-
-
-        
-
-
-
+print(user2.stock_num)
+print(user3.stock_num)
