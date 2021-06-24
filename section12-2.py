e9539a72e7fe3eea9937385614b303cc65ea838a
bumsol2 <bumsol2@gmail.com>
2021-06-24 18:18:48 +0900

    SQL lite 강의

--- section12-2.py
+++ section12-2.py
@@ -4,8 +4,8 @@
 
 import sqlite3
 
-#DB파일 조회(없으면 새로 생성)
-conn = sqlite3.connect('C:/Django/python/resource/database.db') # 내 DB 경로
+# DB파일 조회(없으면 새로 생성)
+conn = sqlite3.connect('C:/Django/python/resource/database.db')  # 내 DB 경로
 
 # 커서 바인딩
 c = conn.cursor()
@@ -28,7 +28,7 @@ print()
 # 순회1
 # rows = c.fetchall()
 # for row in rows:
-#     print('retrieve1 > ', row) 
+#     print('retrieve1 > ', row)
 
 # 순회2
 # for row in c.fetchall():
@@ -42,10 +42,40 @@ print()
 
 # where Retrieve1
 param1 = (3,)
-c.execute('select * from users where id=?',param1)
-print('param1',c.fetchone())
-print('param1',c.fetchall()) # 데이터 없음
+c.execute('select * from users where id=?', param1)
+print('param1', c.fetchone())
+print('param1', c.fetchall())  # 데이터 없음
 
 # where Retrieve2
+param2 = 4
+c.execute('select * from users where id="%s"' % param2)  # %s, %f, %d
+print('param2', c.fetchone())
+print('param2', c.fetchall())  # 데이터 없음
 
+# where Retrieve3
+c.execute('select * from users where id=:Id', {"Id": 5})  # %s, %f, %d
+print('param3', c.fetchone())
+print('param3', c.fetchall())  # 데이터 없음
 
+# Where Retrieve4
+param4 = (3, 5)
+c.execute("select * from users where id IN(?,?)", param4)
+print('param4', c.fetchall())
+
+# Where Retrieve5
+c.execute("select * from users where id IN('%d','%d')" % (3, 4))
+print('param5', c.fetchall())
+
+# Where Retrieve6
+c.execute("select * from users where id =:id1 OR id=:id2",
+          {"id1": 2, "id2": 5})
+print('param5', c.fetchall())
+
+# Dump 출력
+with conn:
+    with open('C:/Django/python/resource/dump.sql', 'w') as f:
+        for line in conn.iterdump():
+            f.write('%s\n' % line)
+        print('Dump Print Complete')
+
+# f.close(), conn.close()
